8a7139a57b0e4f6ee762f7647d76e4cacba9be37
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-03 16:10:00 +0300

    Add create action

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,12 @@
 class JobsController < ApplicationController
-	def new
+	def create
+		@company = Company.find(params[:company_id])
+		@job = @company.jobs.create(job_params)
+		redirect_to company_path(@company)
 	end
+
+	private
+		def job_params
+			params.require(:job).permit(:title, :description)
+		end
 end
